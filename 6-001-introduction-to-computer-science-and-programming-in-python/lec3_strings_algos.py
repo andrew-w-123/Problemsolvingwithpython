f034df29d75f1f8be99e09dc3e275c44aed5fb95
Andrew Wei <andrewswei2015@gmail.com>
2021-03-06 21:47:51 -0800

    Class 3 Lecture Problems

--- 6-001-introduction-to-computer-science-and-programming-in-python/lec3_strings_algos.py
+++ 6-001-introduction-to-computer-science-and-programming-in-python/lec3_strings_algos.py
@@ -17,42 +17,48 @@
 ####################
 #an_letters = "aefhilmnorsxAEFHILMNORSX"
 #word = input("I will cheer for you! Enter a word: ")
+#word = 'Pokemon'
 #times = int(input("Enthusiasm level (1-10): "))
-#
-#i = 0
-#while i < len(word):
-#    char = word[i]
+
+#for char in word:
 #    if char in an_letters:
 #        print("Give me an " + char + "! " + char)
 #    else:
 #        print("Give me a  " + char + "! " + char)
-#    i += 1
 #print("What does that spell?")
 #for i in range(times):
 #    print(word, "!!!")
 
+from datetime import datetime
 
-    
+def printCurrentTime():
+    now = datetime.now()
+    current_time = now.strftime('%H:%M:%S')
+    print("Current Time =", current_time)
+
 ####################
 ## EXAMPLE: perfect cube 
 ####################
+#printCurrentTime()
 #cube = 27
-##cube = 8120601
+#cube = 8120601
 #for guess in range(cube+1):
 #    if guess**3 == cube:
 #        print("Cube root of", cube, "is", guess)
-#        # loops keeps going even after found the cube root
-    
+#        break
+        # loops keeps going even after found the cube root
+#printCurrentTime()
 
 ####################
 ## EXAMPLE: guess and check cube root 
 ####################
+#printCurrentTime()
 #cube = 27
-##cube = 8120601
+#cube = 8120601
 #for guess in range(abs(cube)+1):
-#    # passed all potential cube roots
+    # passed all potential cube roots
 #    if guess**3 >= abs(cube):
-#        # no need to keep searching
+        # no need to keep searching
 #        break
 #if guess**3 != abs(cube):
 #    print(cube, 'is not a perfect cube')
@@ -61,13 +67,14 @@
 #        guess = -guess
 #    print('Cube root of ' + str(cube) + ' is ' + str(guess))
 
-
+#printCurrentTime()
 ####################
 ## EXAMPLE: approximate cube root 
 ####################
 #cube = 27
-##cube = 8120601
-##cube = 10000
+#printCurrentTime()
+#cube = 8120601
+#cube = 10000
 #epsilon = 0.1
 #guess = 0.0
 #increment = 0.01
@@ -82,30 +89,33 @@
 #    print('Failed on cube root of', cube, "with these parameters.")
 #else:
 #    print(guess, 'is close to the cube root of', cube)
+#printCurrentTime()
 
 
 ####################
 ## EXAMPLE: bisection cube root (only positive cubes!)
 ####################
 #cube = 27
-##cube = 8120601
+cube = 8
 ## won't work with x < 1 because initial upper bound is less than ans
-##cube = 0.25
-#epsilon = 0.01
-#num_guesses = 0
-#low = 0
-#high = cube
-#guess = (high + low)/2.0
-#while abs(guess**3 - cube) >= epsilon:
-#    if guess**3 < cube:
-#        # look only in upper half search space
-#        low = guess
-#    else:
-#        # look only in lower half search space
-#        high = guess
-#    # next guess is halfway in search space
-#    guess = (high + low)/2.0
-#    num_guesses += 1
-#print('num_guesses =', num_guesses)
-#print(guess, 'is close to the cube root of', cube)
-   
+#cube = 0.25
+printCurrentTime()
+epsilon = 0.01
+num_guesses = 0
+low = 0
+high = cube
+guess = (high + low)/2.0
+while abs(guess**3 - cube) >= epsilon:
+    if guess**3 < cube:
+        # look only in upper half search space
+        low = guess
+    else:
+        # look only in lower half search space
+        high = guess
+    # next guess is halfway in search space
+    guess = (high + low)/2.0
+    num_guesses += 1
+print('num_guesses =', num_guesses)
+print(guess, 'is close to the cube root of', cube)
+
+printCurrentTime()
